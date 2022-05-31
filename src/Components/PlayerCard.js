efbea2978c74fb99b3e714fd58609e3d3f1bceed
Hanna <petrakovahanna@gmail.com>
2022-05-31 16:09:08 +0200

    graficosfinal

--- src/Components/PlayerCard.js
+++ src/Components/PlayerCard.js
@@ -1,41 +1,185 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Card } from "react-bootstrap";
+import { Card, Container, Col, Row } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import Grafico from "./Grafico";
+import GraficoPie from "./GraficoPie";
 
 const PlayerCard = () => {
   const { player } = useLocation().state;
 
+  const datosgol = [
+    player.goals_selection,
+    player.goal_champ,
+    player.own_goal_champ,
+    player.own_goal_cup,
+    player.goal_cup,
+    player.goal_continent,
+    player.own_goal_continent,
+  ];
+
+  const datostarjetas = [
+    player.red_card_continent,
+    player.yellow_card_champ,
+    player.second_yellow_card_champ,
+    player.red_card_champ,
+    player.yellow_card_cup,
+    player.red_card_cup,
+    player.yellow_card_continent,
+    player.second_yellow_card_continent,
+    player.second_yellow_card_cup,
+  ];
+
+  // states line. GOLS
+  const statesLine = {
+    // etiquetas de días de los datos para el gráfico
+    labels: [
+      "Goals Selection ",
+      "Goal Champ",
+      "Own Goal Champ ",
+      " Own Goal Cup",
+      "Goal Cup",
+      "Goal Continent",
+      "Own Goal Continent",
+    ],
+    datasets: [
+      {
+        label: "Goals",
+        // cifras para el gráfico
+        data: datosgol,
+
+        tension: 0.2,
+        fill: true,
+        backgroundColor: "#6979F8",
+        pointBorderColor: "#6979F8",
+        pointBackgroundColor: "#6979F8",
+        pointRadius: 5,
+      },
+    ],
+  };
+
+  // array con las opciones de formato al imprimir el gráfico
+  const chartOptionsLine = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+
+      scales: {
+        x: {
+          offset: true,
+        },
+      },
+
+      title: {
+        text: "Goals",
+        display: false,
+        fontSize: 20,
+      },
+    },
+  };
+
+  // STATES PIE TARJETAS
+  const statesPie = {
+    // etiquetas de días de los datos para el gráfico
+    labels: [
+      "Red Card Continent",
+      "Yellow Card Champ",
+      "Second Yellow Card Champ",
+      "Red Card Champ",
+      "Yellow Card Cup",
+      "Red Card Cup",
+      "Yellow Card Continent",
+      "Second Yellow Card Continent",
+      "Second Yellow Card Cup",
+    ],
+    datasets: [
+      {
+        label: "Graphic",
+        // cifras para el gráfico
+        data: datostarjetas,
+
+        tension: 0.2,
+        fill: true,
+        backgroundColor: [
+          "#ff7f50",
+          "#87cefa",
+          "#da70d6",
+          "#32cd32",
+          "#6495ed",
+          "#ff69b4",
+          "#ba55d3",
+          "#cd5c5c",
+          "#ffa500",
+        ],
+        pointBorderColor: "#6979F8",
+        pointBackgroundColor: "#6979F8",
+        pointRadius: 5,
+      },
+    ],
+  };
+
+  // array con las opciones de formato al imprimir el gráfico
+  const chartOptionsPie = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+
+      scales: {
+        x: {
+          offset: true,
+        },
+      },
+
+      title: {
+        display: false,
+        fontSize: 20,
+      },
+    },
+  };
+
   return (
-    <Card>
-      <Card.Title>ID: {player?.id}</Card.Title>
-      <Card.Text>Red Card Continent: {player?.red_card_continent}</Card.Text>
-      <Card.Text>Age: {player?.age}</Card.Text>
-      <Card.Text>Nation {player?.nation}</Card.Text>
-      <Card.Text>League {player?.league}</Card.Text>
-      <Card.Text>Team {player?.team}</Card.Text>
-      <Card.Text>Goals Selection {player?.goals_selection}</Card.Text>
-      <Card.Text>Selections Nation {player?.selections_nation}</Card.Text>
-      <Card.Text>Position {player?.position}</Card.Text>
-      <Card.Text>Price {player?.price}</Card.Text>
-      <Card.Text>Goal Champ {player?.goal_champ}</Card.Text>
-      <Card.Text>Assist Champ {player?.assist_champ}</Card.Text>
-      <Card.Text>Own Goal Champ {player?.own_goal_champ}</Card.Text>
-      <Card.Text>Yellow Card Champ {player?.yellow_card_champ}</Card.Text>
-      <Card.Text>Second Yellow Card Champ {player?.second_yellow_card_champ}</Card.Text>
-      <Card.Text>Red Card Champ {player?.red_card_champ}</Card.Text>
-      <Card.Text>Goal Cup {player?.goal_cup}</Card.Text>
-      <Card.Text>Assist Cup {player?.assist_cup}</Card.Text>
-      <Card.Text>Own Goal Cup {player?.own_goal_cup}</Card.Text>
-      <Card.Text>Yellow Card Cup {player?.yellow_card_cup}</Card.Text>
-      <Card.Text>Second Yellow Card Cup {player?.second_yellow_card_cup}</Card.Text>
-      <Card.Text>Red Card Cup {player?.red_card_cup}</Card.Text>
-      <Card.Text>Goal Continent {player?.goal_continent}</Card.Text>
-      <Card.Text>Assist Continent {player?.assist_continent}</Card.Text>
-      <Card.Text>Own Goal Continent {player?.own_goal_continent}</Card.Text>
-      <Card.Text>Yellow Card Continent {player?.yellow_card_continent}</Card.Text>
-      <Card.Text>Second Yellow Card Continent {player?.second_yellow_card_continent}</Card.Text>
-    </Card>
+    <Container>
+      <div className="big-box-players">
+        <Row className="m-5">
+          <Col>
+            <Row>
+              <h1 style={{ fontWeight: "bolder" }}>ID: {player?.id}</h1>
+              <h2>Price {player?.price}</h2>
+            </Row>
+          </Col>
+          <Col>
+            <h2>Information</h2>
+            <p>Age: {player?.age}</p>
+            <p>Position {player?.position}</p>
+            <p>Nation {player?.nation}</p>
+            <p> Team {player?.team}</p>
+          </Col>
+        </Row>
+        <Row className="p-3">
+          <h2> Goals </h2>
+          <Grafico options={chartOptionsLine} data={statesLine} />
+        </Row>
+
+        <Row className="m-5">
+          <Col>
+            <h2>Statistics</h2>
+
+            <p> Team {player?.team}</p>
+            <p> Selections Nation {player?.selections_nation}</p>
+            <p> Assist Champ {player?.assist_champ}</p>
+            <p> Assist Cup {player?.assist_cup}</p>
+            <p>Assist Continent {player?.assist_continent}</p>
+          </Col>
+
+          <Col>
+            <h2> Cards </h2>
+            <GraficoPie options={chartOptionsPie} data={statesPie} />
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 };
 
