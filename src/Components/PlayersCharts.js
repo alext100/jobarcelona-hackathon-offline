ca3bd7aa9d73205cef3cd4499bf451692aeacd2f
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-05-31 15:55:48 +0200

    Class name ChartExample renamed to StackedBarCharts

--- src/Components/PlayersCharts.js
+++ src/Components/PlayersCharts.js
@@ -2,14 +2,14 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 // eslint-disable-next-line import/no-cycle
-import ChartExample from "./StackedBarCharts";
+import StackedBarCharts from "./StackedBarCharts";
 
 const PlayersCharts = () => {
   const { playersToCompare } = useLocation().state;
 
   return (
     <div className="charts-container" style={{ height: "70vh" }}>
-      <ChartExample data={playersToCompare} />
+      <StackedBarCharts data={playersToCompare} />
     </div>
   );
 };
