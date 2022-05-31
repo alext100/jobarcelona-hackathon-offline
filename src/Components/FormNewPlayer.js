475597397984e7eeb33e154405447cfb714edc6c
Marc <riokost@gmail.com>
2022-05-31 15:18:26 +0200

    form analyze styles

--- src/Components/FormNewPlayer.js
+++ src/Components/FormNewPlayer.js
@@ -47,129 +47,142 @@ const FormNewPlayer = () => {
   };
 
   return (
-    <InputGroup className="mb-3 form-input-group">
-      <Container style={{ width: "60vh" }}>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3">
-                <Form.Label>Age</Form.Label>
-                <Form.Control
-                  value={age}
-                  id="age"
-                  onChange={handleChange}
-                  type="number"
-                  min="16"
-                  max="60"
-                  placeholder="Enter age"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" >
-                <Form.Label>Goals Selection</Form.Label>
-                <Form.Control
-                  value={goals_selection}
-                  id="goals_selection"
-                  onChange={handleChange}
-                  type="number"
-                  min="0"
-                  placeholder="Goals Selection"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" >
-                <Form.Label>Selections Nation</Form.Label>
-                <Form.Control
-                  value={selections_nation}
-                  onChange={handleChange}
-                  id="selections_nation"
-                  type="number"
-                  min="0"
-                  placeholder="Selections Nation"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Goal Champ</Form.Label>
-                <Form.Control
-                  value={goal_champ}
-                  id="goal_champ"
-                  onChange={handleChange}
-                  type="number"
-                  min="0"
-                  placeholder="Goal Champ"
-                  required
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3">
-                <Form.Label>Goal Cup</Form.Label>
-                <Form.Control
-                  value={goal_cup}
-                  id="goal_cup"
-                  onChange={handleChange}
-                  type="number"
-                  min="0"
-                  placeholder="Goal Cup"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Goal Continent</Form.Label>
-                <Form.Control
-                  value={goal_continent}
-                  id="goal_continent"
-                  onChange={handleChange}
-                  type="number"
-                  min="0"
-                  placeholder="Goal Continent"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Enter your price</Form.Label>
-                <Form.Control
-                  value={price}
-                  id="price"
-                  onChange={handleChange}
-                  type="number"
-                  min="0"
-                  step="1000"
-                  placeholder="Enter your price"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Position</Form.Label>
-                <Form.Select
-                  aria-label="Select player position"
-                  id="position"
-                  onChange={handleChange}>
-                  <option>Choose player position</option>
-                  <option value="Goalkeeper">Goalkeeper</option>
-                  <option value="LeftWinger">LeftWinger</option>
-                  <option value="RightWinger">RightWinger</option>
-                  <option value="LeftMidfield">LeftMidfield</option>
-                  <option value="RightMidfield">RightMidfield</option>
-                  <option value="DefensiveMidfield">DefensiveMidfield</option>
-                  <option value="CentralMidfield">CentralMidfield</option>
-                  <option value="SecondStriker">SecondStriker</option>
-                  <option value="Defender">Defender</option>
-                  <option value="Midfielder">Midfielder</option>
-                  <option value="AttackingMidfield">AttackingMidfield</option>
-                  <option value="Forward">Forward</option>
-                </Form.Select>
-              </Form.Group>
-            </Col>
-            <Button variant="primary" type="submit" onClick={handleShow}>
-              Analyze
-            </Button>
-          </Row>
-        </Form >
-        <ModalAnalyze show={show} onHide={handleClose} data={player} />
-      </Container >
-    </InputGroup >
+    <>
+      <Row>
+        <Col md={12}>
+          <h1 className="titleAnalyze">Analyze a player</h1>
+        </Col>
+      </Row>
+      <InputGroup className="mb-3 form-input-group centerInputGroup">
+        <Row>
+          <Col md={12}>
+            <Container style={{ width: "60vh" }}>
+              <div>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Row>
+                    <Col>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control
+                          value={age}
+                          id="age"
+                          onChange={handleChange}
+                          type="number"
+                          min="16"
+                          max="60"
+                          placeholder="Enter age"
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" >
+                        <Form.Label>Goals Selection</Form.Label>
+                        <Form.Control
+                          value={goals_selection}
+                          id="goals_selection"
+                          onChange={handleChange}
+                          type="number"
+                          min="0"
+                          placeholder="Goals Selection"
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" >
+                        <Form.Label>Selections Nation</Form.Label>
+                        <Form.Control
+                          value={selections_nation}
+                          onChange={handleChange}
+                          id="selections_nation"
+                          type="number"
+                          min="0"
+                          placeholder="Selections Nation"
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Goal Champ</Form.Label>
+                        <Form.Control
+                          value={goal_champ}
+                          id="goal_champ"
+                          onChange={handleChange}
+                          type="number"
+                          min="0"
+                          placeholder="Goal Champ"
+                          required
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Goal Cup</Form.Label>
+                        <Form.Control
+                          value={goal_cup}
+                          id="goal_cup"
+                          onChange={handleChange}
+                          type="number"
+                          min="0"
+                          placeholder="Goal Cup"
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Goal Continent</Form.Label>
+                        <Form.Control
+                          value={goal_continent}
+                          id="goal_continent"
+                          onChange={handleChange}
+                          type="number"
+                          min="0"
+                          placeholder="Goal Continent"
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Enter your price</Form.Label>
+                        <Form.Control
+                          value={price}
+                          id="price"
+                          onChange={handleChange}
+                          type="number"
+                          min="0"
+                          step="1000"
+                          placeholder="Enter your price"
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Position</Form.Label>
+                        <Form.Select
+                          aria-label="Select player position"
+                          id="position"
+                          onChange={handleChange}>
+                          <option>Choose player position</option>
+                          <option value="Goalkeeper">Goalkeeper</option>
+                          <option value="LeftWinger">LeftWinger</option>
+                          <option value="RightWinger">RightWinger</option>
+                          <option value="LeftMidfield">LeftMidfield</option>
+                          <option value="RightMidfield">RightMidfield</option>
+                          <option value="DefensiveMidfield">DefensiveMidfield</option>
+                          <option value="CentralMidfield">CentralMidfield</option>
+                          <option value="SecondStriker">SecondStriker</option>
+                          <option value="Defender">Defender</option>
+                          <option value="Midfielder">Midfielder</option>
+                          <option value="AttackingMidfield">AttackingMidfield</option>
+                          <option value="Forward">Forward</option>
+                        </Form.Select>
+                      </Form.Group>
+                    </Col>
+                    <Button variant="primary" type="submit" onClick={handleShow}>
+                      Analyze
+                    </Button>
+                  </Row>
+                </Form >
+                <ModalAnalyze show={show} onHide={handleClose} data={player} />
+              </div>
+            </Container >
+          </Col>
+        </Row>
+      </InputGroup >
+    </>
   );
 };
 
